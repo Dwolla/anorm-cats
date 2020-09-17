32297e937b91e7d1ad19ee3733f2b233c078e2b6
Brian P. Holt <bholt@dwolla.com>
2020-09-17 17:52:12 -0500

    switch to travisci because I don't have time to figure out github actions right now

---
+++ .travis/release.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset
+
+USERNAME="Dwolla Bot"
+RELEASE_BRANCH="main"
+
+commit_username=$(git log -n1 --format=format:"%an")
+if [[ "$commit_username" == "$USERNAME" ]]; then
+  echo "Refusing to release a commit created by this script."
+  exit 0
+fi
+
+if [ "$TRAVIS_BRANCH" != "${RELEASE_BRANCH}" ]; then
+  echo "Only the ${RELEASE_BRANCH} branch will be released. This branch is $TRAVIS_BRANCH."
+  exit 0
+fi
+
+git config user.name "$USERNAME"
+git config user.email "dev+dwolla-bot@dwolla.com"
+
+git remote add release "https://$GH_TOKEN@github.com/Dwolla/fs2-aws.git"
+git fetch release
+
+git clean -dxf
+git checkout ${RELEASE_BRANCH}
+git branch --set-upstream-to=release/${RELEASE_BRANCH}
+
+MAIN_BRANCH=$(git rev-parse HEAD)
+if [ "$TRAVIS_COMMIT" != "$MAIN_BRANCH" ]; then
+  echo "Checking out ${RELEASE_BRANCH} set HEAD to $MAIN_BRANCH, but Travis was building $TRAVIS_COMMIT, so refusing to continue."
+  exit 0
+fi
+
+sbt clean "release with-defaults"
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
 addSbtPlugin("org.foundweekends" % "sbt-bintray" % "0.5.6")
 addSbtPlugin("com.github.gseitz" % "sbt-release" % "1.0.11")
 addSbtPlugin("io.github.davidgregory084" % "sbt-tpolecat" % "0.1.13")
-addSbtPlugin("com.codecommit" % "sbt-github-actions" % "0.8.1")
+addSbtPlugin("com.dwijnand" % "sbt-travisci" % "1.2.0")
